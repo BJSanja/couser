221b1b91d035e060411abfd972f58df150df0912
K. A. H. Bimsara Jinith Sanja <87575102+BJSanja@users.noreply.github.com>
2023-01-22 06:53:31 +0530

    Add files via upload
    java code

---
+++ Example.java
@@ -0,0 +1,30 @@
+/*public class Example{
+	 public static void main(String []args){
+		
+		System.out.println('t'+'g');
+		System.out.print(6+1*5+"34"+2*3);
+		
+	}
+
+}*/
+
+class Example {
+ public static void main(String [] args) {
+	int x = 3;
+    while (x > 0) {	
+	if (x > 2) {
+ 	    System.out.print("a");
+ 	}
+	x = x - 1;
+ 	System.out.print("-");
+	if (x == 2) {
+ 	    System.out.print("b c");
+ 	}
+	if (x == 1) {
+	    System.out.print("d");
+	     x = x - 1;
+	 }
+     }
+ }
+}
+	
